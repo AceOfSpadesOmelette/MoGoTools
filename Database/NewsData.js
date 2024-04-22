ac9c837a75be219143ebb33a7b63e03676197332
AceOfSpadesOmelette <165576195+AceOfSpadesOmelette@users.noreply.github.com>
2024-04-22 23:19:12 +0800

    Add files via upload

---
+++ Database/NewsData.js
@@ -0,0 +1,7 @@
+export const NEWS_DATA = [
+    {
+        "NewsTime": "1713155983",
+        "NewsHeader": "First Public Release",
+        "NewsContent": "First public release of MoGoTools!<br>Updated album to Making Music."
+    },
+]
