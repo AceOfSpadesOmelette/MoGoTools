b10187d0dc87b8e58bcdbbbd06277582dc461964
AceOfSpadesOmelette <165576195+AceOfSpadesOmelette@users.noreply.github.com>
2024-04-24 17:12:05 +0800

    Update NewsData.js

--- Database/NewsData.js
+++ Database/NewsData.js
@@ -9,4 +9,9 @@ export const NEWS_DATA = [
         "NewsHeader": "New feature added: show sticker art when not owned",
         "NewsContent": `Added the feature to choose whether sticker art is displayed when they are not obtained.<br>To change the setting, go to Menu > Display, then change "Show sticker art when not obtained" to ON.`
     },
+    {
+        "NewsTime": "1713948921",
+        "NewsHeader": "Name Change to Making Music Set 10 #3 Sticker",
+        "NewsContent": `The name of Set 10 #3 sticker has been changed from "Pickin' Girl" to "Banjo Pluckin".`
+    },
 ]
