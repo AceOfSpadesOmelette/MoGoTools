a69d4f4b693afbaae7de1874b4ea80bdffb06067
AceOfSpadesOmelette <165576195+AceOfSpadesOmelette@users.noreply.github.com>
2024-04-24 20:55:06 +0800

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -245,7 +245,7 @@ function ApplySelectedStyle(container) {
   if (userDataItem.selected === 0 && StickerSelectedZeroShowOneBack === 1) {
     if(WebZeroMobileOne === 0){container.querySelector('.spare-spinner-container').style.marginTop = '53.5px';}
     else if(WebZeroMobileOne === 1){
-      container.querySelector('.spare-spinner-container').style.marginTop = '55%';
+      container.querySelector('.spare-spinner-container').style.marginTop = '53.5%';
       container.querySelector('.sticker-ribbon-transparent').style.marginTop = '-90%';
     }
   }
