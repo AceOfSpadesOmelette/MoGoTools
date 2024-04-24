d7bf198bbd0bfc8d5a78ee12cf42cb2d3edb2d4d
AceOfSpadesOmelette <165576195+AceOfSpadesOmelette@users.noreply.github.com>
2024-04-24 12:29:41 +0800

    Update NewsData.js

--- Database/NewsData.js
+++ Database/NewsData.js
@@ -1,7 +1,12 @@
 export const NEWS_DATA = [
     {
         "NewsTime": "1713155983",
-        "NewsHeader": "First Public Release",
-        "NewsContent": "First public release of MoGoTools!<br>Updated album to Making Music."
+        "NewsHeader": "First Beta Release",
+        "NewsContent": "First beta release of MoGoTools!<br>Updated album to Making Music."
+    },
+    {
+        "NewsTime": "1713931930",
+        "NewsHeader": "New feature added: show sticker art when not owned",
+        "NewsContent": `Added the feature to choose whether sticker art is displayed when they are not obtained.<br>To change the setting, go to Menu > Display, then change "Show sticker art when not obtained" to ON.`
     },
 ]
