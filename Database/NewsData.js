e60450994fec6f1fec22541857d7f37dbe7d758f
AceOfSpadesOmelette <165576195+AceOfSpadesOmelette@users.noreply.github.com>
2024-04-25 17:21:00 +0800

    Update NewsData.js

--- Database/NewsData.js
+++ Database/NewsData.js
@@ -7,6 +7,11 @@ export const NEWS_DATA = [
     {
         "NewsTime": "1713931930",
         "NewsHeader": "New feature added: show sticker art when not owned",
-        "NewsContent": `Added the feature to choose whether sticker art is displayed when they are not obtained.<br>To change the setting, go to Menu > Display, then change "Show sticker art when not obtained" to ON.`
+        "NewsContent": "Added the feature to choose whether sticker art is displayed when they are not obtained.<br>To change the setting, go to Menu > Display, then change \"Show sticker art when not obtained\" to ON."
     },
+    {
+        "NewsTime": "1714036372",
+        "NewsHeader": "Quick Set Scrolling Feature Added!",
+        "NewsContent": "Added the ability to quickly scroll between sets.<br>Simply click on the set icon to automatically scroll to the first sticker that matches the set, if there are any."
+    }
 ]
